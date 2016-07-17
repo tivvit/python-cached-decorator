2cddb54a224ddc021e41c8d75bdebbd80e40b395
Vit Listik <tivvit@seznam.cz>
2016-07-17 23:12:52 +0200

    PEP 8

--- Cached.py
+++ Cached.py
@@ -1,13 +1,13 @@
-__author__ = 'tivvit'
-
 import time
 import pickle
 import os
 
+__author__ = 'tivvit'
 
-def _rename(newname):
+
+def _rename(new_name):
     def decorator(f):
-        f.__name__ = newname
+        f.__name__ = new_name
         return f
 
     return decorator
@@ -36,15 +36,18 @@ class Cached(object):
         # rename is used because other decorators using __name__
         @_rename(self.name)
         def wrapped_f(*args, **kwargs):
-            self.name_params = self.name + "_" + str(hash(str(args) + "|" + str(kwargs)))
+            self.name_params = self.name + "_" + str(
+                hash(str(args) + "|" + str(kwargs)))
 
             if not self.force_run:
                 return_value = self.__load()
 
             if self.force_run or not return_value:
                 return_value = f(*args, **kwargs)
-                out_file_name = self.name_params + "_" + str(time.time()) + ".pkl"
-                with open(os.path.join(self.output_path, out_file_name), "wb+") as out_file:
+                out_file_name = self.name_params + "_" + str(
+                    time.time()) + ".pkl"
+                with open(os.path.join(self.output_path, out_file_name),
+                          "wb+") as out_file:
                     pickle.dump(return_value, out_file)
 
             return return_value
@@ -54,16 +57,20 @@ class Cached(object):
     def __load(self):
         import glob
 
-        files = glob.glob(os.path.join(self.output_path, self.name_params + "*"))
+        files = glob.glob(
+            os.path.join(self.output_path, self.name_params + "*"))
         if files:
             newest_file = max(files)
             with open(newest_file, "rb") as out:
                 from datetime import datetime
 
-                create_time = float(newest_file.replace(".pkl", "").split("_")[-1])
-                created = datetime.fromtimestamp(create_time).strftime("%d.%m.%Y %H:%M:%S")
+                create_time = float(
+                    newest_file.replace(".pkl", "").split("_")[-1])
+                created = datetime.fromtimestamp(create_time).strftime(
+                    "%d.%m.%Y %H:%M:%S")
                 print "loading %s (created %s)" % (newest_file, created)
                 return pickle.load(out)
         else:
-            print "%s_*.pkl not found (maybe different args). RUNNING" % os.path.join(self.output_path,
-                                                                                      self.name_params)
+            print "%s_*.pkl not found (maybe different args). RUNNING" % os.path.join(
+                self.output_path,
+                self.name_params)
